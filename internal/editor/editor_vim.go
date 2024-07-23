0fb1770071bba0526c432590680068eb0be298ee
Damien Dart <damiendart@pobox.com>
2024-07-23 23:28:33 +0100

    "editor_vim.go": Do some tweakin'.
    Add initial support for launching Vim using process replacement.

--- internal/editor/editor_vim.go
+++ internal/editor/editor_vim.go
@@ -10,6 +10,9 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"runtime"
+	"strings"
+	"syscall"
 )
 
 // VimEditor is a representation of the Vim text editor.
@@ -30,29 +33,58 @@ func (editor VimEditor) OpenFile(path string, w io.Writer, root string) error {
 		return err
 	}
 
-	if root != "" {
-		err := os.Chdir(root)
+	// Windows does not support the "execve(2)" system call.
+	if runtime.GOOS == "windows" {
+		if root != "" {
+			err := os.Chdir(root)
+			if err != nil {
+				return err
+			}
+		}
+
+		cmd := exec.Command("vim", quotePath(path))
+		cmd.Env = os.Environ()
+		cmd.Stderr = os.Stderr
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+
+		err := cmd.Run()
 		if err != nil {
+			var exitError *exec.ExitError
+
+			if errors.As(err, &exitError) {
+				return fmt.Errorf("vim exited with exit status %d", exitError.ExitCode())
+			}
+
 			return err
 		}
+
+		return nil
 	}
 
-	cmd := exec.Command("vim", path)
-	cmd.Env = os.Environ()
-	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
+	shell := os.Getenv("SHELL")
+	if len(shell) == 0 {
+		shell = "sh"
+	}
 
-	err := cmd.Run()
+	shellPath, err := exec.LookPath(shell)
 	if err != nil {
-		var exitError *exec.ExitError
-
-		if errors.As(err, &exitError) {
-			return fmt.Errorf("vim exited with exit status %d", exitError.ExitCode())
-		}
-
 		return err
 	}
 
-	return nil
+	cmd := ""
+
+	if root == "" {
+		cmd = fmt.Sprintf("vim %s", quotePath(path))
+	} else {
+		cmd = fmt.Sprintf(`vim --cmd 'cd %s' %s`, root, quotePath(path))
+	}
+
+	return syscall.Exec(shellPath, []string{shellPath, "-c", cmd}, os.Environ())
+}
+
+func quotePath(path string) string {
+	r := strings.NewReplacer("'", "'\\''")
+
+	return "'" + r.Replace(path) + "'"
 }
